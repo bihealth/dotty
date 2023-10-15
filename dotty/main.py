2b425bfacffb918643999b84379d2893159f91eb
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2023-10-15 09:22:27 +0200

    feat: Endpoint for retrieving all transcripts for a given HGNC ID (#11) (#12)
    Co-authored-by: Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>

--- dotty/main.py
+++ dotty/main.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 from contextlib import asynccontextmanager
 
 import bioutils.assemblies
@@ -15,6 +16,10 @@ _logger = logging.getLogger(__name__)
 
 #: The global Driver instance.
 driver: Driver = None  # type: ignore[assignment]
+#: Map from HGNC ID to transcripts.
+hgnc_to_transcripts: dict[str, list[typing.Any]] = {}
+#: Map from Assembly to map from hgnc_id to transcripts.
+assembly_to_hgnc_to_transcripts: dict[Assembly, dict[str, list[typing.Any]]] = {}
 
 #: Contig names per assembly.
 contig_names: dict[Assembly, set[str]] = {
@@ -32,6 +37,21 @@ async def lifespan(app: FastAPI):  # pragma: no cover
     driver = Driver(cdot_dir=settings.DATA_DIR)
     driver.load()
     _logger.info("driver loaded")
+    for assembly in Assembly:
+        for transcript in driver.data_providers[assembly].transcripts.keys():
+            if (
+                assembly.value
+                not in driver.data_providers[assembly]
+                ._get_transcript(transcript)["genome_builds"]
+                .keys()
+            ):
+                continue
+            hgnc_id = f"HGNC:{driver.data_providers[assembly]._get_transcript(transcript)['hgnc']}"
+            hgnc_to_transcripts.setdefault(hgnc_id, []).append(
+                driver.data_providers[assembly]._get_transcript(transcript)
+            )
+        assembly_to_hgnc_to_transcripts[assembly] = hgnc_to_transcripts
+    _logger.info("map built")
     yield
 
 
@@ -61,6 +81,93 @@ class Result(pydantic.BaseModel):
 
     #: The actual payload / SPDI representation of the variant.
     spdi: Spdi
+
+
+class ExonAlignment(pydantic.BaseModel):
+    """Alignment of an exon to an assembly."""
+
+    #: Exon start in reference.
+    ref_start: int
+    #: Exon end in reference.
+    ref_end: int
+    #: Exon number.
+    exon_no: int
+    #: Exon start in transcript.
+    tx_start: int
+    #: Exon end in transcript.
+    tx_end: int
+    #: The gapped alignment description.
+    alignment: str | None
+
+    @staticmethod
+    def _from_list(lst: list[typing.Any]) -> "ExonAlignment":
+        """Create an ``ExonAlignment`` from a list."""
+        return ExonAlignment(
+            ref_start=lst[0],
+            ref_end=lst[1],
+            exon_no=lst[2],
+            tx_start=lst[3],
+            tx_end=lst[4],
+            alignment=lst[5],
+        )
+
+
+class TanscriptAlignment(pydantic.BaseModel):
+    """Alignment of a `Transcript` to an assembly."""
+
+    #: Assembly of alignment.
+    assembly: str
+    #: Alignment contig.
+    contig: str
+    #: CDS start.
+    cds_start: int
+    #: CDS end.
+    cds_end: int
+    #: Exons, first two entries are start/end positions on the chromosome.
+    exons: list[ExonAlignment]
+
+    @staticmethod
+    def _from_dict(assembly, dct: dict[str, typing.Any]) -> "TanscriptAlignment":
+        """Create a ``TanscriptAlignment`` from a dictionary."""
+        return TanscriptAlignment(
+            assembly=assembly,
+            contig=dct["contig"],
+            cds_start=dct["cds_start"],
+            cds_end=dct["cds_end"],
+            exons=[ExonAlignment._from_list(lst) for lst in dct["exons"]],
+        )
+
+
+class Transcript(pydantic.BaseModel):
+    """Transcript model."""
+
+    #: Transcript ID.
+    id: str
+    #: Gene HGNC ID.
+    hgnc_id: str
+    #: Gene HGNC symbol.
+    hgnc_symbol: str
+    #: Alignments of the transcripts.
+    alignments: list[TanscriptAlignment]
+
+    @staticmethod
+    def _from_dict(assembly: str, dct: dict[str, typing.Any]) -> "Transcript":
+        """Create a ``Transcript`` from a dictionary."""
+        return Transcript(
+            id=dct["id"],
+            hgnc_id=f"HGNC:{dct['hgnc']}",
+            hgnc_symbol=dct["gene_name"],
+            alignments=[TanscriptAlignment._from_dict(assembly, dct["genome_builds"][assembly])]
+            if assembly in dct["genome_builds"]
+            else [],
+        )
+
+
+class TranscriptResult(pydantic.BaseModel):
+    """The result of the query for searching for transcripts."""
+
+    #: The actual payload / list of transcripts.
+    transcripts: list[Transcript]
 
 
 @app.get("/api/v1/to-spdi", response_model=Result)
@@ -93,3 +200,24 @@ async def to_spdi(q: str, assembly: Assembly = Assembly.GRCH38) -> Result:
             alternate_inserted=alternative,
         )
     )
+
+
+@app.get("/api/v1/find-transcripts", response_model=TranscriptResult)
+async def find_transcripts(hgnc_id: str, assembly: Assembly = Assembly.GRCH38) -> TranscriptResult:
+    """Find transcripts for the given HGNC ID."""
+    result = []
+    transctipts = assembly_to_hgnc_to_transcripts[assembly].get(hgnc_id, [])
+    if not transctipts:
+        raise HTTPException(status_code=404, detail="No transcripts found")
+    else:
+        for t in transctipts:
+            if (
+                assembly.value not in t["genome_builds"]
+                or "cds_start" not in t["genome_builds"][assembly.value]
+                or "cds_end" not in t["genome_builds"][assembly.value]
+                or "exons" not in t["genome_builds"][assembly.value]
+            ):
+                continue
+
+            result.append(Transcript._from_dict(assembly.value, t))
+        return TranscriptResult(transcripts=result)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
+import json
+import typing
 from unittest.mock import Mock
 
 from _pytest.monkeypatch import MonkeyPatch
 from fastapi.testclient import TestClient
+from pytest_snapshot.plugin import Snapshot
 
 from dotty import main as dotty_main
 from dotty.core import Assembly
@@ -39,6 +42,33 @@ def _setup_mock_driver(var_type: str, parsed_var_ac: str) -> Mock:
     mock_driver.parser.parse.return_value = parsed_var
 
     return mock_driver
+
+
+def _setup_mock_transcript_data() -> typing.Dict[Assembly, typing.Any]:
+    hgnc_to_transcripts_grch37: dict[str, list[typing.Any]] = {}
+    assembly_to_hgnc_to_transcripts_grch37: dict[Assembly, dict[str, list[typing.Any]]] = {}
+
+    transcript_info_grch37 = json.load(open("tests/data/cdot-0.2.21.refseq.grch37.json", "r"))
+    for transcript in transcript_info_grch37["transcripts"].values():
+        hgnc_id = f"HGNC:{transcript['hgnc']}"
+        hgnc_to_transcripts_grch37.setdefault(hgnc_id, []).append(transcript)
+    assembly_to_hgnc_to_transcripts_grch37[Assembly.GRCH37] = hgnc_to_transcripts_grch37
+
+    hgnc_to_transcripts_grch38: dict[str, list[typing.Any]] = {}
+    assembly_to_hgnc_to_transcripts_grch38: dict[Assembly, dict[str, list[typing.Any]]] = {}
+
+    transcript_info_grch38 = json.load(open("tests/data/cdot-0.2.21.refseq.grch38.json", "r"))
+    for transcript in transcript_info_grch38["transcripts"].values():
+        hgnc_id = f"HGNC:{transcript['hgnc']}"
+        hgnc_to_transcripts_grch38.setdefault(hgnc_id, []).append(transcript)
+    assembly_to_hgnc_to_transcripts_grch38[Assembly.GRCH38] = hgnc_to_transcripts_grch38
+
+    assembly_to_hgnc_to_transcripts = {
+        **assembly_to_hgnc_to_transcripts_grch37,
+        **assembly_to_hgnc_to_transcripts_grch38,
+    }
+
+    return assembly_to_hgnc_to_transcripts
 
 
 def test_to_spdi_c(test_client: TestClient, monkeypatch: MonkeyPatch):
@@ -103,3 +133,29 @@ def test_to_spdi_g_38(test_client: TestClient, monkeypatch: MonkeyPatch):
         }
     }
     assert response.json() == expected
+
+
+def test_find_transcripts_grch37(
+    test_client: TestClient, monkeypatch: MonkeyPatch, snapshot: Snapshot
+):
+    monkeypatch.setattr(dotty_main, "driver", _setup_mock_driver("g", "NC_000017.11"))
+    monkeypatch.setattr(
+        dotty_main, "assembly_to_hgnc_to_transcripts", _setup_mock_transcript_data()
+    )
+    response = test_client.get("/api/v1/find-transcripts?hgnc_id=HGNC:1100&assembly=GRCh37")
+    assert response.status_code == 200
+
+    snapshot.assert_match(json.dumps(response.json(), indent=2), "response.json")
+
+
+def test_find_transcripts_grch38(
+    test_client: TestClient, monkeypatch: MonkeyPatch, snapshot: Snapshot
+):
+    monkeypatch.setattr(dotty_main, "driver", _setup_mock_driver("g", "NC_000017.11"))
+    monkeypatch.setattr(
+        dotty_main, "assembly_to_hgnc_to_transcripts", _setup_mock_transcript_data()
+    )
+    response = test_client.get("/api/v1/find-transcripts?hgnc_id=HGNC:1100&assembly=GRCh38")
+    assert response.status_code == 200
+
+    snapshot.assert_match(json.dumps(response.json(), indent=2), "response.json")
